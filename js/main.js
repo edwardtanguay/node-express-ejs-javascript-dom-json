d92f21d3677f015451c48b1d6cbf63b90464a8f6
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-08 19:54:22 +0100

    shows text

--- js/main.js
+++ js/main.js
@@ -3,4 +3,12 @@ import { EmployeesManager } from './employeesManager.js';
 const messageElem = document.querySelector('.message');
 const employeesManager = new EmployeesManager();
 
-messageElem.innerHTML = employeesManager.getMessage();
+messageElem.innerHTML = 'loading...';
+
+(async () => {
+	const response = await fetch('http://localhost:3100');
+	// const data = await response.json();
+	const data = await response.text();
+	messageElem.innerHTML = data;
+
+})();
