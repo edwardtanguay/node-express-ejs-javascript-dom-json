755db15609925d4f2969894335709df29a9ca7ba
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-09 06:58:13 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
 const messageElem = document.querySelector('.message');
+const newMessageElem = document.querySelector('.newMessage');
+const btnChange = document.querySelector('.btnChange');
 
 const emulationLoadTimeInSeconds = 1000;
+
 messageElem.innerHTML = '<span class="loading">loading...</span>';
+newMessageElem.value = '';
+
+const setFormToLoading = () => {
+	newMessageElem.disabled = true;
+	btnChange.disabled = true;
+}
+
+const setFormToReady = (data) => {
+	messageElem.innerHTML = data.message;
+	newMessageElem.value = data.message;
+	newMessageElem.disabled = false;
+	btnChange.disabled = false;
+}
+
+setFormToLoading();
+
+btnChange.addEventListener('click', (e) => {
+	e.preventDefault();
+	setFormToLoading();
+	const requestOptions = {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ message: newMessageElem.value })
+	};
+	setTimeout(() => {
+		(async () => {
+			const response = await fetch('http://localhost:3100', requestOptions);
+			const data = await response.json();
+			setFormToReady(data);
+		})();
+	}, emulationLoadTimeInSeconds);
+});
 
 setTimeout(() => {
 	(async () => {
 		const response = await fetch('http://localhost:3100');
 		const data = await response.json();
-		messageElem.innerHTML = data.message;
+		setFormToReady(data);
 	})();
 }, emulationLoadTimeInSeconds);
